8a73898dbd00e7a9a6af4783af763bc235f31e46
micahmura21 <Mickmura21@gmail.com>
2017-07-10 17:05:56 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+var arr0 = ["painting0","painting1","painting2","painting3","painting4"]
+var arr1 = ["genji0.jpg","starwars4.jpg","hanzoart2.jpg","modernart3","pbuyart"]
+var arr2 = ["genji art this is my favortie art","starwars art is cool too i guess...","hanzoart is cool as ice","if you feel lazy and just mash a whole bunch of sricbbles then thats some eazy money","zenytta is coolest of cats."]
